4ee013a5340933bf54ba718e7ebbc4769806c1ed
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-11-10 10:29:26 -0300

    feat: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,22 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { dark } from "@clerk/themes"
+import { dark } from "@clerk/themes";
+import Navbar from "./_components/navbar";
 
 const Home = async () => {
   const { userId } = await auth();
   if (!userId) {
-    redirect('/login')
+    redirect("/login");
   }
   return (
-    <div className="flex h-full items-center justify-center">
-      <UserButton
-        showName
-        appearance={{ baseTheme: dark }}
-      />
-    </div>
+    <>
+      <Navbar />
+      <div className="flex h-full items-center justify-center">
+        <UserButton showName appearance={{ baseTheme: dark }} />
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
