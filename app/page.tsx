771fa266afc267e59746a768a117f351f3623655
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-11-10 12:15:17 -0300

    feat: add summary cards on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,36 @@
-import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { dark } from "@clerk/themes";
 import Navbar from "./_components/navbar";
+import SummaryCards from "./(home)/_components/summary-cards";
+import TimeSelect from "./(home)/_components/time-select";
+import { isMatch } from "date-fns";
 
-const Home = async () => {
+interface HomePage {
+  searchParams: {
+    month: string;
+  };
+}
+
+const Home = async ({ searchParams: { month } }: HomePage) => {
   const { userId } = auth();
+
+  const monthIsInvalid = !month || !isMatch(month, "MM");
+
+  if (monthIsInvalid) {
+    redirect("?month=01");
+  }
   if (!userId) {
     redirect("/login");
   }
   return (
     <>
       <Navbar />
-      <div className="flex h-full items-center justify-center">
-        <UserButton showName appearance={{ baseTheme: dark }} />
+      <div className="space-y-6 p-6">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
+        </div>
+        <SummaryCards month={month} />
       </div>
     </>
   );
