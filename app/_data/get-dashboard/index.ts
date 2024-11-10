c7dc54e5967765706596493ca0727ff86617836b
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-11-10 15:14:25 -0300

    feat: expenses per category summary on dashboard

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -1,6 +1,9 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
-import { TransactionPercentagePerType } from "./types";
+import {
+  TotalExpensesPerCategory,
+  TransactionPercentagePerType,
+} from "./types";
 import { auth } from "@clerk/nextjs/server";
 
 export const getDashboard = async (month: string) => {
@@ -59,36 +62,36 @@ export const getDashboard = async (month: string) => {
       (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
     ),
   };
-  // const totalExpensePerCategory: TotalExpensePerCategory[] = (
-  //   await db.transaction.groupBy({
-  //     by: ["category"],
-  //     where: {
-  //       ...where,
-  //       type: TransactionType.EXPENSE,
-  //     },
-  //     _sum: {
-  //       amount: true,
-  //     },
-  //   })
-  // ).map((category) => ({
-  //   category: category.category,
-  //   totalAmount: Number(category._sum.amount),
-  //   percentageOfTotal: Math.round(
-  //     (Number(category._sum.amount) / Number(expensesTotal)) * 100,
-  //   ),
-  // }));
-  // const lastTransactions = await db.transaction.findMany({
-  //   where,
-  //   orderBy: { date: "desc" },
-  //   take: 15,
-  // });
+  const totalExpensePerCategory: TotalExpensesPerCategory[] = (
+    await db.transaction.groupBy({
+      by: ["category"],
+      where: {
+        ...where,
+        type: TransactionType.EXPENSE,
+      },
+      _sum: {
+        amount: true,
+      },
+    })
+  ).map((category) => ({
+    category: category.category,
+    totalAmount: Number(category._sum.amount),
+    percentageOfTotal: Math.round(
+      (Number(category._sum.amount) / Number(expensesTotal)) * 100,
+    ),
+  }));
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: { date: "desc" },
+    take: 15,
+  });
   return {
     balance,
     depositsTotal,
     investmentsTotal,
     expensesTotal,
     typesPercentage,
-    // totalExpensePerCategory,
-    // lastTransactions,
+    totalExpensePerCategory,
+    lastTransactions,
   };
 };
