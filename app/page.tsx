ac56e72df7df72a0aff40271715b6ec9bfbf0665
Lucas Bernardo <lucasbeniti.bernardo@gmail.com>
2024-11-10 13:56:39 -0300

    feat: add balance summary pie chart on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Navbar from "./_components/navbar";
 import SummaryCards from "./(home)/_components/summary-cards";
 import TimeSelect from "./(home)/_components/time-select";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./(home)/_components/transactions-pie-chart";
+import { getDashboard } from "./_data/get-dashboard";
 
 interface HomePage {
   searchParams: {
@@ -22,6 +24,7 @@ const Home = async ({ searchParams: { month } }: HomePage) => {
   if (!userId) {
     redirect("/login");
   }
+  const dashboard = await getDashboard(month);
   return (
     <>
       <Navbar />
@@ -30,7 +33,14 @@ const Home = async ({ searchParams: { month } }: HomePage) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
